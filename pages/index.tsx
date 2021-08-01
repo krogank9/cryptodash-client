eb77a4e2227a550adc2d5f1bad0bd1394367fbf3
krogank9 <wifi.mouse.app@gmail.com>
2021-07-31 23:45:16 -0400

    remove overview in favor of index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
-import Overview from './Overview'
+import css from './Overview.module.scss'
+
+import WalletCarousel from '../components/WalletCarousel/WalletCarousel'
+import OverviewGraph from '../components/OverviewGraph/OverviewGraph'
+import PricesTable from '../components/PricesTable/PricesTable'
+import IonIcon from '../components/IonIcon/IonIcon'
+import TrendingCurrenciesTable from '../components/TrendingCurrenciesTable/TrendingCurrenciesTable'
+import CryptoNewsfeed from '../components/CryptoNewsfeed/CryptoNewsfeed'
 
 //-------------------------------------------------------------
 // Serverside static/initial props -- updated with cron job that fetches coingecko api
@@ -42,6 +49,19 @@ export async function getServerSideProps(context) {
 
 //-------------------------------------------------------------
 
-export default function Home(args) {
-  return Overview(args)
+export default function Overview(props) {
+  return (
+    <>
+      <WalletCarousel />
+      <div className={css.graphSplit}>
+        <OverviewGraph className={css.graphSplit__graph} />
+        <PricesTable className={css.graphSplit__table} data={props.marketData}/>
+      </div>
+
+      <div className={css.halfSplit}>
+        <TrendingCurrenciesTable />
+        <CryptoNewsfeed />
+      </div>
+    </>
+  )
 }
